ad9a77d119b25a3c10eda02421a7ff53ece0b0db
Ashish Poudel <62178291+ashishpoudel523@users.noreply.github.com>
2022-06-20 23:37:24 +0545

    Update Mask Training.py

--- Mask Training.py
+++ Mask Training.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from imutils import paths
 
+
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import Dropout
